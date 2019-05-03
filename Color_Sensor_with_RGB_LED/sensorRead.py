9cd0500f87b2b2e0ad4f085257fc6896e7e4c0ab
zalkar-z <zalkar@bennington.edu>
2019-05-03 20:19:42 +0000

    Sensor manual initialization started

--- Color_Sensor_with_RGB_LED/sensorRead.py
+++ Color_Sensor_with_RGB_LED/sensorRead.py
@@ -1,11 +1,13 @@
-import board
-import busio
-import time
-import adafruit_tcs34725
-i2c = busio.I2C(board.SCL, board.SDA)
-sensor = adafruit_tcs34725.TCS34725(i2c)
+# import board
+# import busio
+# import time
+# import adafruit_tcs34725
+# i2c = busio.I2C(board.SCL, board.SDA)
+# sensor = adafruit_tcs34725.TCS34725(i2c)
 
 
-def get_color():
-	return sensor.color_rgb_bytes
+# def get_color():
+#	return sensor.color_rgb_bytes
+
+SENSOR_ADDRESS = 0x29
 
