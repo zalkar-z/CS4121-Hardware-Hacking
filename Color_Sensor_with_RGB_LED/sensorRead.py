d52f56b63411a4c8ee9883d5a05a10e4a5169ccf
zalkar-z <zalkar@bennington.edu>
2019-05-04 21:59:56 +0000

    Sensor enabled

--- Color_Sensor_with_RGB_LED/sensorRead.py
+++ Color_Sensor_with_RGB_LED/sensorRead.py
@@ -9,5 +9,21 @@
 # def get_color():
 #	return sensor.color_rgb_bytes
 
-SENSOR_ADDRESS = 0x29
 
+import smbus
+import time
+
+SENSOR_ADDRESS = 0x29
+bus = smbus.SMBus(1)
+
+# print(bus.read_byte_data(0x16, 0b1010011))
+
+bus.write_byte_data(SENSOR_ADDRESS, 0x00, 0x01)
+bus.write_byte_data(SENSOR_ADDRESS, 0x00, 0x03)
+
+# RED HIGH
+print(bus.read_byte_data(SENSOR_ADDRESS, 0b10010111))
+
+
+# GREEN HIGH
+print(bus.read_byte_data(SENSOR_ADDRESS, 0b10011001))
