16b504eace177d0e87e645158687662b91cc3bb2
zalkar-z <zalkar@bennington.edu>
2019-05-05 20:50:35 +0000

    sensorRead.py cleaned up and commented

--- Color_Sensor_with_RGB_LED/sensorRead.py
+++ Color_Sensor_with_RGB_LED/sensorRead.py
@@ -1,38 +1,46 @@
 import smbus
 import time
 
+
+# set device address
 SENSOR_ADDRESS = 0x29
+
+# user bus#1
 bus = smbus.SMBus(1)
 
+# read a color from the sensor
 def get_color():
 	# enable register - 0X03
 	bus.write_byte_data(SENSOR_ADDRESS, 0x00, 0b00000011)
 
-	# CLEAR LOW
+	# for all [Clear, Red, Green, Blue] below, we have to read LOW state first, and then HIGH state. 
+	# Since, according to the manafacturer's datasheet, it would give us the most accurate value
+	# Once we get HIGH state, we don't care about LOW state
+	# NOTE: for all colors, LOW and HIGH states have consequent addresses
+
+	# CLEAR
 	clear = bus.read_byte_data(SENSOR_ADDRESS, 0b10010100)
-	clear_high = bus.read_byte_data(SENSOR_ADDRESS, 0b10010101)
+	clear = bus.read_byte_data(SENSOR_ADDRESS, 0b10010101)
 
-	# RED LOW
+	# RED
 	red = bus.read_byte_data(SENSOR_ADDRESS, 0b10010110)
-	red_high = bus.read_byte_data(SENSOR_ADDRESS, 0b10010111)
+	red = bus.read_byte_data(SENSOR_ADDRESS, 0b10010111)
 
-	# GREEN LOW
+	# GREEN
 	green = bus.read_byte_data(SENSOR_ADDRESS, 0b10011000)
-	green_high = bus.read_byte_data(SENSOR_ADDRESS, 0b10011001)
+	green = bus.read_byte_data(SENSOR_ADDRESS, 0b10011001)
 
-	# BLUE LOW
+	# BLUE
 	blue = bus.read_byte_data(SENSOR_ADDRESS, 0b10011010)
-	blue_high = bus.read_byte_data(SENSOR_ADDRESS, 0b10011011)
-
-	# temporary
-	clear = clear_high
-
+	blue = bus.read_byte_data(SENSOR_ADDRESS, 0b10011011)
+	
+	# if the color is black
 	if clear == 0:
 		red = green = blue = 0
 	else:
-		red = red_high / clear * 255.0
-		green = green_high / clear * 255.0
-		blue = blue_high / clear * 255.0
+		red = red / clear * 255.0
+		green = green / clear * 255.0
+		blue = blue / clear * 255.0
 
 	return [red, green, blue]
 
